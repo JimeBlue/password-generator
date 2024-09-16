95d15fdeac18394c358dc92d0bf8030072ae792f
Jimena <j.miguez@airlst.com>
2024-09-16 14:43:32 +0200

    add basic inputs

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,33 @@
       <h1 class="text-2xl font-bold text-brand-500">
         {{ $t('welcome.title') }}
       </h1>
-      <p class="mt-4" />
+      <UForm
+        class="space-y-4"
+      >
+        <UFormGroup label="Character Length" name="passwordLength">
+          <URange />
+        </UFormGroup>
+
+        <UFormGroup label="Include Uppercase Letters" name="includeUppercase">
+          <UCheckbox />
+        </UFormGroup>
+
+        <UFormGroup label="Include Lowercase Letters" name="includeLowercase">
+          <UCheckbox />
+        </UFormGroup>
+
+        <UFormGroup label="Include Numbers" name="includeNumbers">
+          <UCheckbox />
+        </UFormGroup>
+
+        <UFormGroup label="Include Symbols" name="includeSymbols">
+          <UCheckbox />
+        </UFormGroup>
+
+        <UButton type="submit">
+          Generate
+        </UButton>
+      </UForm>
     </div>
   </section>
 </template>
